98dae38bdae1da3e8da88cab54d885d0dd49abce
Alessandro Stein <alessandro@alessandrostein.com>
2015-04-24 19:12:08 -0400

    Update sete_segments.c

--- sete_segments.c
+++ sete_segments.c
@@ -1,15 +1,20 @@
 void main() {
+  ADCON1 = 0x07;
+  
+  TRISA.RA2 = 0; // Saída
+  TRISA.RA3 = 0; // Saída
+  TRISA.RA4 = 0; // Saída
+  TRISA.RA5 = 0; // Saída
+  
+  TRISD = 0X00; // Saída
 
- ADCON1 = 0x07;
- TRISD = 1;
+  PORTA.RA2 = 1; // Módulo Display Ligado
+  PORTA.RA3 = 1; // Módulo Display Ligado
+  PORTA.RA4 = 0; // Módulo Display Desligado
+  PORTA.RA5 = 1; // Módulo Display Ligado
 
- TRISD = 1; // Saída
-
- PORTD.RD2 = 1; // Display Ligado
- PORTD.RD3 = 0; // Display Desligado
- PORTD.RD4 = 0; // Display Desligado
- PORTD.RD5 = 0; // Display Desligado
-
- PORTD = 0b01100110;
- //        hgfedcba
+  PORTD = 0b10000111;
+  //        hgfedcba
+  // Pode-se configurar também em Hexadecimal, por exemplo
+  // PORTD = 0x07
 }
